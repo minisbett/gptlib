727d34b2f060ec9e21bfd23011f72ace3d69b643
minisbett <39670899+minisbett@users.noreply.github.com>
2023-08-22 14:58:58 +0200

    Remove static date models

--- gptlib/Enums/ModelEnum.cs
+++ gptlib/Enums/ModelEnum.cs
@@ -15,27 +15,12 @@ public enum Model
   [EnumMember(Value = "gpt-3.5-turbo")] GPT3_5_TURBO,
 
   /// <summary>
-  /// The static 1st march version of the gpt-3.5-turbo model.
-  /// </summary>
-  [EnumMember(Value = "gpt-3.5-turbo-0301")] GPT3_5_TURBO_0301,
-
-  /// <summary>
   /// The gpt-4 model for the 8K context.
   /// </summary>
   [EnumMember(Value = "gpt-4")] GPT4_8K,
 
   /// <summary>
-  /// The static 14th march version of the gpt-4 model.
-  /// </summary>
-  [EnumMember(Value = "gpt-4-0314")] GPT4_0314,
-
-  /// <summary>
   /// The gpt-4 model for the 32K context.
   /// </summary>
   [EnumMember(Value = "gpt-4-32k")] GPT4_32K,
-
-  /// <summary>
-  /// The static 14th march version of the gpt-4-32k model.
-  /// </summary>
-  [EnumMember(Value = "gpt-4-32k-0314")] GPT4_32K_0314
 }
