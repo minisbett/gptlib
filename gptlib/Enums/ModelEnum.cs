05510361a6af04a8015e3d817be09b7add2f02ca
minisbett <39670899+minisbett@users.noreply.github.com>
2023-08-22 17:43:14 +0200

    Updated static models

--- gptlib/Enums/ModelEnum.cs
+++ gptlib/Enums/ModelEnum.cs
@@ -15,12 +15,27 @@ public enum Model
   [EnumMember(Value = "gpt-3.5-turbo")] GPT3_5_TURBO,
 
   /// <summary>
+  /// The static 13th june version of the gpt-3.5-turbo model.
+  /// </summary>
+  [EnumMember(Value = "gpt-3.5-turbo-0613")] GPT3_5_TURBO_0613,
+
+  /// <summary>
   /// The gpt-4 model for the 8K context.
   /// </summary>
   [EnumMember(Value = "gpt-4")] GPT4_8K,
 
   /// <summary>
+  /// The static 13th june version of the gpt-4 model.
+  /// </summary>
+  [EnumMember(Value = "gpt-4-0613")] GPT4_0613,
+
+  /// <summary>
   /// The gpt-4 model for the 32K context.
   /// </summary>
   [EnumMember(Value = "gpt-4-32k")] GPT4_32K,
+
+  /// <summary>
+  /// The static 13th june version of the gpt-4-32k model.
+  /// </summary>
+  [EnumMember(Value = "gpt-4-32k-0613")] GPT4_32K_0613
 }
